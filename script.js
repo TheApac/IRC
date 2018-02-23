2c3ed44b53baa3779af96e5105c37ef09f94530b
TheApac <apac.alexcuevas@gmail.com>
2018-02-23 19:44:08 +0100

    Début main page

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function OpenSignIn() {
   $(".passwordConfirm").hide("slow");
-  $("input:submit").attr("value","Connexion");
+  $("input:submit").attr("value", "Connexion");
   $("h1").html("Merci de vous connecter avant de pouvoir utiliser le chat en direct");
+  $("h1").
   $("p").html('Vous ne possédez pas compte ? <button class="black-text" onclick="OpenLogIn()">Inscrivez vous</button>');
 }
 
 function OpenLogIn() {
   $(".passwordConfirm").show("slow");
-  $("button").attr("onclick","OpenSignIn()");
-  $("input:submit").attr("value","Inscription");
+  $("button").attr("onclick", "OpenSignIn()");
+  $("input:submit").attr("value", "Inscription");
   $("h1").html("Merci de créer un compte avant de pouvoir utiliser le chat en direct");
   $("p").html('Vous possédez un compte ? <button class="black-text" onclick="OpenSignIn()">Connectez vous</button>');
 }
