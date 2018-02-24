163e0da90b2a31b1769e1be2e85a7b538d90455f
Joris <joris.chalumeau@gmail.com>
2018-02-24 23:08:17 +0100

    angular index.html-script.js

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-function OpenSignIn() {
+var app = angular.module('indexApp',[]);
+app.controller('indexCtrl', function($scope){
+  $scope.username = '';
+  $scope.password = '';
+  $scope.passwordConfirm = '';
+  $scope.isLogIn = false;
+  $scope.switchLogInSignIn = function() {
+    $scope.isLogIn = !$scope.isLogIn;
+  }
+});
+
+function OpenLogIn() {
   $(".passwordConfirm").hide("slow");
   $("input:submit").attr("value", "Connexion");
   $("h1").html("Merci de vous connecter avant de pouvoir utiliser le chat en direct");
   $("p").html('Vous ne possédez pas compte ? <button class="black-text" onclick="OpenLogIn()">Inscrivez vous</button>');
 }
 
-function OpenLogIn() {
+function OpenSignIn() {
   $(".passwordConfirm").show("slow");
   $("button").attr("onclick", "OpenSignIn()");
   $("input:submit").attr("value", "Inscription");
   $("h1").html("Merci de créer un compte avant de pouvoir utiliser le chat en direct");
   $("p").html('Vous possédez un compte ? <button class="black-text" onclick="OpenSignIn()">Connectez vous</button>');
 }
+
+
+/* socket functions :
+
+principe :
+socket.emit('truc qui est recu sur le serveur', [parametre1, parametre2, ...., parametre n]);
+
+pour la connexion :
+socket.emit('tryConnect', [$("input[type=text]").val(), $("input[type=password]").val()]);
+
+pour la deconnexion :
+onbeforeunload
+*/
