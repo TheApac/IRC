471c15d646a8062a97516e7615d98adc9c6558c3
TheApac <apac.alexcuevas@gmail.com>
2018-03-01 00:40:15 +0100

    Debug + ajout trigger fonction quand touche Entrée utilisée

--- main-script.js
+++ main-script.js
@@ -1,170 +1,170 @@
 var mainApp = angular.module('mainApp', ['ngAnimate', 'ngCookies']);
-mainApp.controller('mainCtrl', function($scope, $cookies){
-  console.log($cookies.get('user'));
-   if ($cookies.get('user') === undefined)
+mainApp.controller('mainCtrl', function($scope, $cookies) {
+  if ($cookies.get('user') === undefined)
     location.href = "/index.html";
-   $scope.main = {};
-   $scope.model = {};
-   $scope.main.user = $cookies.get('user'); // current user
-   $scope.main.channelTabs = ""; // list of opened channels
-   $scope.main.message = ""; // current message typed by user
-   $scope.main.listUsersInChannel = {}; // list of connected users in each channel
-   $scope.main.listMsgInChannel = {}; // list of msg displayed in the channel since current user's connection
-   $scope.model.availableChannelsList = []; // list of all opened channels in the server
-   $scope.model.writeChannels = false;
-   $scope.model.selectedChannel = "";
+  $scope.main = {};
+  $scope.model = {};
+  $scope.main.user = $cookies.get('user'); // current user
+  $scope.main.channelTabs = ""; // list of opened channels
+  $scope.main.message = ""; // current message typed by user
+  $scope.main.listUsersInChannel = {}; // list of connected users in each channel
+  $scope.main.listMsgInChannel = {}; // list of msg displayed in the channel since current user's connection
+  $scope.model.availableChannelsList = []; // list of all opened channels in the server
+  $scope.model.writeChannels = false;
+  $scope.model.selectedChannel = "";
 
-   socket.emit("getChannels");
+  socket.emit("getChannels");
 
-   // lorsqu'on se déconnecte du serveur
-   $scope.deconnexion = function() {
-     $cookies.remove('user');
-     location.href = ('/index.html');
-     // socet.emit('deconnexion', $scope.main.user)
-   };
-   // lorsqu'on clique sur un autre tab
-   $scope.switchTab = function(tabName) {
-     socket.emit('')
-   };
-   // lorsqu'on ouvre la fenetre pour rejoindre un autre channel
-   $scope.joinTab = function() {
-     // socket.emit('')
-   };
-   // lorsqu'on envoie un messages
-   $scope.sendMessage = function() {
-     if (left($scope.main.message, 1) == '/') {
-       $scope.execCommand(main.message);
-     } else {
-       socket.emit('NewMsg', [msg, main.user, current_date_time]);
-     }
-     // on vide inputMessages
-     main.message = "";
-   };
-   // pour parser et executer la commande
-   $scope.execCommand = function() {
-     var tmpMsg = main.message.split(" ");
-     if (len(tmpMsg) < 1)
-       return;
-     switch (tmpMsg[0]) {
-       case "/pseudo":
-         if(len(tmpMsg) == 2) {
-           // socket.emit('renamePseudo', [$scope.main.user, tmpMsg[1]]);
-           // $scope.main.user = tmpMsg[1]; //TODO in socket.on('changedPseudo')
-         } else {
-           $scope.dispErrorCmd();
-         }
-         break;
-       case "/channels":
-         if(len(tmpMsg) == 1) {
-           socket.emit('getChannels', [$scope.main.user]);
-           $scope.model.writeChannels = true;
-         } else {
-           $scope.dispErrorCmd();
-         }
-         break;
-       case "/disconnect":
-         if(len(tmpMsg) == 1) {
-           $scope.deconnexion();
-         } else {
-           $scope.dispErrorCmd();
-         }
-         break;
-       case "/delete":
-         if(len(tmpMsg) == 2 && tmpMsg[1] in $scope.main.listUsersInTab.keys) {
-           // socet.emit('DeletingChannel', [$scope.main.user, tmpMsg[1]])
-         } else {
-           $scope.dispErrorCmd();
-         }
-         break;
-       case "/rename":
-         if(len(tmpMsg) == 3 && tmpMsg[1] in $scope.main.listUsersInTab.keys) {
-           // socet.emit('renameChan', [$scope.main.user, tmpMsg[1], tmpMsg[2]])
-         } else {
-           $scope.dispErrorCmd();
-         }
-         break;
-       case "/leave":
-         if(len(tmpMsg) == 2 && tmpMsg[1] in $scope.main.listUsersInTab.keys) {
-           socket.emit('deconnexionChannel', [tmpMsg[1], $scope.main.user])
-         } else {
-           $scope.dispErrorCmd();
-         }
-         break;
-       case "/join":
-         if(len(tmpMsg) == 2 && tmpMsg[1] in $scope.main.listUsersInTab.keys) {
-           // socet.emit('joinChan', [$scope.main.user, tmpMsg[1]])
-         } else {
-           $scope.dispErrorCmd();
-         }
-         break;
-       case "/help":
-         alert("'/pseudo name' to rename your pseudo into 'name'\n"
-              +"'/channels' to display the list of all existing channels\n"
-              +"'/disconnect' to disconnect from the server\n"
-              +"'/delete chan' to remove the channel 'chan'\n"
-              +"'/rename chan name' to rename channel 'chan' into 'name'\n"
-              +"'/leave chan' to leave channel 'chan'\n"
-              +"'/join chan' to join channel 'chan'");
-         break;
-       default:
-         $scope.dispErrorCmd();
-     }
-   };
-   $scope.dispErrorCmd = function() {
-     var errorMsg = "Command syntaxe error : "+$scope.main.message+"\n";
-     errorMsg += "type '/help' to display the list of available commands";
-     // write errorMsg in chat (in red)
-     alert(errorMsg);
-   };
-   socket.on("ChannelCreated", function() {
-     var tabs = $scope.main.channelTabs;
-     if(tabs == "")
-       tabs = "<ul class='tabs'>";
-     else {
-       tabs.slice(0, -5); // remove '</ul>'
-     }
-     var newTab = "<li class='tab col m2'><a href='#"+tabName+"'>"+tabName+"</a></li>";
-     $scope.main.channelTabs = tabs+newTab+'</ul>';
-   });
-   socket.on("ChannelExists", function() {
-     alert("channel name already used");
-   });
-   socket.on("deconnexionChannel", function(param){
-     if (param[1] == $scope.main.user) {
-       // l'utilisateur actuel vient de se déco du channel param[0]
-       $scope.main.channelTabs.splice(param[0]);
-       delete $scope.main.listUsersInChannel[param[0]];
-       delete $scope.main.listMsgInChannel[param[0]];
-     } else {
-       // un autre utilisateur vient de se déco du channel param[0]
-       $scope.main.listUsersInChannel[param[0]].splice(param[1]);
-       // TODO write msg: param[1]+" left  the channel"
-     }
-   });
-   socket.on("ChannelNames", function(channels) {
-     $scope.model.availableChannelsList = [];
-     msg = "";
-     for(chan in channels) {
-       $scope.model.availableChannelsList.push(chan.name);
-       if($scope.model.writeChannels) {
-         msg = msg + ", " + chan.name;
-       }
-     }
-     if($scope.model.writeChannels) {
-       // TODO write msg: msg
-       $scope.model.writeChannels = false;
-     }
-   });
-   socket.on("newMessage", function(params) {
-     // params = [content, user, date_time]
-     // TODO write msg: params
-     if (params[1] == $scope.main.user) {
-       // write on the right
-     } else {
-       // write on the left
-     }
-   });
- });
+  // lorsqu'on se déconnecte du serveur
+  $scope.deconnexion = function() {
+    $cookies.remove('user');
+    location.href = ('/index.html');
+    // socet.emit('deconnexion', $scope.main.user)
+  };
+  // lorsqu'on clique sur un autre tab
+  $scope.switchTab = function(tabName) {
+    socket.emit('')
+  };
+  // lorsqu'on ouvre la fenetre pour rejoindre un autre channel
+  $scope.joinTab = function() {
+    // socket.emit('')
+  };
+  // lorsqu'on envoie un messages
+  $scope.sendMessage = function() {
+      var tmpMsg = $scope.main.message;
+    if (tmpMsg.substring(0, 1) == '/') {
+      $scope.execCommand(tmpMsg);
+    } else {
+      socket.emit('NewMsg', [tmpMsg, $scope.main.user, current_date_time]);
+    }
+    // on vide inputMessages
+    main.message = "";
+  };
+  // pour parser et executer la commande
+  $scope.execCommand = function() {
+    var tmpMsg = main.message.split(" ");
+    if (len(tmpMsg) < 1)
+      return;
+    switch (tmpMsg[0]) {
+      case "/pseudo":
+        if (len(tmpMsg) == 2) {
+          // socket.emit('renamePseudo', [$scope.main.user, tmpMsg[1]]);
+          // $scope.main.user = tmpMsg[1]; //TODO in socket.on('changedPseudo')
+        } else {
+          $scope.dispErrorCmd();
+        }
+        break;
+      case "/channels":
+        if (len(tmpMsg) == 1) {
+          socket.emit('getChannels', [$scope.main.user]);
+          $scope.model.writeChannels = true;
+        } else {
+          $scope.dispErrorCmd();
+        }
+        break;
+      case "/disconnect":
+        if (len(tmpMsg) == 1) {
+          $scope.deconnexion();
+        } else {
+          $scope.dispErrorCmd();
+        }
+        break;
+      case "/delete":
+        if (len(tmpMsg) == 2 && tmpMsg[1] in $scope.main.listUsersInTab.keys) {
+          // socet.emit('DeletingChannel', [$scope.main.user, tmpMsg[1]])
+        } else {
+          $scope.dispErrorCmd();
+        }
+        break;
+      case "/rename":
+        if (len(tmpMsg) == 3 && tmpMsg[1] in $scope.main.listUsersInTab.keys) {
+          // socet.emit('renameChan', [$scope.main.user, tmpMsg[1], tmpMsg[2]])
+        } else {
+          $scope.dispErrorCmd();
+        }
+        break;
+      case "/leave":
+        if (len(tmpMsg) == 2 && tmpMsg[1] in $scope.main.listUsersInTab.keys) {
+          socket.emit('deconnexionChannel', [tmpMsg[1], $scope.main.user])
+        } else {
+          $scope.dispErrorCmd();
+        }
+        break;
+      case "/join":
+        if (len(tmpMsg) == 2 && tmpMsg[1] in $scope.main.listUsersInTab.keys) {
+          // socet.emit('joinChan', [$scope.main.user, tmpMsg[1]])
+        } else {
+          $scope.dispErrorCmd();
+        }
+        break;
+      case "/help":
+        alert("'/pseudo name' to rename your pseudo into 'name'\n" +
+          "'/channels' to display the list of all existing channels\n" +
+          "'/disconnect' to disconnect from the server\n" +
+          "'/delete chan' to remove the channel 'chan'\n" +
+          "'/rename chan name' to rename channel 'chan' into 'name'\n" +
+          "'/leave chan' to leave channel 'chan'\n" +
+          "'/join chan' to join channel 'chan'");
+        break;
+      default:
+        $scope.dispErrorCmd();
+    }
+  };
+  $scope.dispErrorCmd = function() {
+    var errorMsg = "Command syntaxe error : " + $scope.main.message + "\n";
+    errorMsg += "type '/help' to display the list of available commands";
+    // write errorMsg in chat (in red)
+    alert(errorMsg);
+  };
+  socket.on("ChannelCreated", function() {
+    var tabs = $scope.main.channelTabs;
+    if (tabs == "")
+      tabs = "<ul class='tabs'>";
+    else {
+      tabs.slice(0, -5); // remove '</ul>'
+    }
+    var newTab = "<li class='tab col m2'><a href='#" + tabName + "'>" + tabName + "</a></li>";
+    $scope.main.channelTabs = tabs + newTab + '</ul>';
+  });
+  socket.on("ChannelExists", function() {
+    alert("channel name already used");
+  });
+  socket.on("deconnexionChannel", function(param) {
+    if (param[1] == $scope.main.user) {
+      // l'utilisateur actuel vient de se déco du channel param[0]
+      $scope.main.channelTabs.splice(param[0]);
+      delete $scope.main.listUsersInChannel[param[0]];
+      delete $scope.main.listMsgInChannel[param[0]];
+    } else {
+      // un autre utilisateur vient de se déco du channel param[0]
+      $scope.main.listUsersInChannel[param[0]].splice(param[1]);
+      // TODO write msg: param[1]+" left  the channel"
+    }
+  });
+  socket.on("ChannelNames", function(channels) {
+    $scope.model.availableChannelsList = [];
+    msg = "";
+    for (chan in channels) {
+      $scope.model.availableChannelsList.push(chan.name);
+      if ($scope.model.writeChannels) {
+        msg = msg + ", " + chan.name;
+      }
+    }
+    if ($scope.model.writeChannels) {
+      // TODO write msg: msg
+      $scope.model.writeChannels = false;
+    }
+  });
+  socket.on("newMessage", function(params) {
+    // params = [content, user, date_time]
+    // TODO write msg: params
+    if (params[1] == $scope.main.user) {
+      // write on the right
+    } else {
+      // write on the left
+    }
+  });
+});
 
 /* socket functions :
 
